10cdfa0826cd677e464f4ac411f5b218debdf08c
DhominickJ <dhominickjohn.billena@wvsu.edu.ph>
2023-03-08 09:35:14 +0800

    Added While Loop

--- app.py
+++ app.py
@@ -11,16 +11,17 @@ def main():
     st.title("Midterm Exam in CCS221")
     func = st.selectbox("Function Choices",options=['DDALine' , 'Flood Fill', 'Boundary Fill', 'Open CV', '3D Shapes'])
     if (func == 'DDALine'):
-        st.header("Task 1")
-        st.subheader("DDA Line Algorithm")
-        x1 = st.slider("Enter X1: ", 1, 100, 1) #slider for the x1 coordinate
-        y1 = st.slider("Enter Y1: ", 1, 100, 1) #slider for the y1 coordinate
-        x2 = st.slider("Enter X2: ", 1, 100, 1) #slider for the x2 coordinate
-        y2 = st.slider("Enter Y2: ", 1, 100, 1) #slider for the y2 coordinate
-        color = "r." #red color for the plotted line
-        color_midpoint = "b." #the color of the midpoint will be blue
-        choice = st.selectbox("Function", options = ["DDALine", "Brensenham"])
-        act1.DDALine (x1, y1, x2, y2, color, color_midpoint)
+        while(True):
+            st.header("Task 1")
+            st.subheader("DDA Line Algorithm")
+            x1 = st.slider("Enter X1: ", 1, 100, 1) #slider for the x1 coordinate
+            y1 = st.slider("Enter Y1: ", 1, 100, 1) #slider for the y1 coordinate
+            x2 = st.slider("Enter X2: ", 1, 100, 1) #slider for the x2 coordinate
+            y2 = st.slider("Enter Y2: ", 1, 100, 1) #slider for the y2 coordinate
+            color = "r." #red color for the plotted line
+            color_midpoint = "b." #the color of the midpoint will be blue
+            choice = st.selectbox("Function", options = ["DDALine", "Brensenham"])
+            act1.DDALine (x1, y1, x2, y2, color, color_midpoint)
     elif (func == 'Flood Fill'):
         st.write("To be Fixed")
     elif (func == 'Boundary Fill'):
